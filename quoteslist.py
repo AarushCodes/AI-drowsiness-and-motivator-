3554d19c2d1a54dacdc21553e248e15ae92e0374
AarushCodes <75005702+AarushCodes@users.noreply.github.com>
2023-12-25 21:58:18 +0530

    Add files via upload

---
+++ quoteslist.py
@@ -0,0 +1,24 @@
+anger_quotes = [
+    "In times of great stress or adversity, it's always best to keep busy, to plow your anger and your energy into something positive.",
+    "Your calm mind is the ultimate weapon against your challenges. So relax.",
+    "Anybody can become angry — that is easy, but to be angry with the right person and to the right degree and at the right time and for the right purpose, and in the right way — that is not within everybody's power and is not easy.",
+    "For every minute you remain angry, you give up sixty seconds of peace of mind.",
+    "In times of anger, don't let emotions control your actions. Pause and reflect.",
+    "Anger is an acid that can do more harm to the vessel in which it is stored than to anything on which it is poured.",
+    "Whenever you feel like criticizing anyone, just remember that all the people in this world haven't had the advantages that you've had.",
+    "Speak when you are angry, and you will make the best speech you will ever regret.",
+    "Anger is often what pain looks like when it shows itself in public.",
+    "Let today be the day you stop being haunted by the ghost of yesterday. Holding a grudge and harboring anger/resentment is poison to the soul."
+]
+sadness_quotes = [
+    "Every morning brings new potential, but only if you make the most of it. You can't change the past, but you can learn from it, and you can shape the future.",
+    "It's okay not to be okay as long as you are not giving up.",
+    "Tough times never last, but tough people do.",
+    "Your value does not decrease based on someone's inability to see your worth.",
+    "The only way to do great work is to love what you do.",
+    "The sun himself is weak when he first rises, and gathers strength and courage as the day gets on.",
+    "No one can make you feel inferior without your consent.",
+    "Don't be discouraged. It's often the last key in the bunch that opens the lock.",
+    "You are stronger than you think, more capable than you know, and more loved than you can imagine.",
+    "Success is not final, failure is not fatal: It is the courage to continue that counts."
+]
